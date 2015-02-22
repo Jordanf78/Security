63e3cae2f47e3ba9c968527b952392a29f69b0d6
Jordanf78 <jflag002@plattsburhgh.edu>
2015-02-22 13:01:57 -0500

    Updated Login.py
    Took out simplecrypt and made a simple encryption function, fixed calls
    and loops.  runs fine on its own now, ready to be incorporated.

--- login.py
+++ login.py
@@ -1,7 +1,8 @@
-from simplecrypt import encrypt, decrypt
+import os.path
 
 def main():
-    if input("(1) Login (2) Create User: ") == 1:
+    create_userfile()
+    if input("(1) Login (2) Create User: ") == "1":
         user = input("Username:")
         password = input ("Password:")
         if login(user, password) == True:
@@ -11,23 +12,41 @@ def main():
     else:
         user = input("Username:")
         password = input ("Password:")
-        create_user(username, password)
+        create_user(user, password)
         main()
 
+def create_userfile():
+    if not os.path.exists(r'Program Data\Users.txt'):
+        with open(r'Program Data\Users.txt', 'w') as fout:
+            fout.write('')
+
 def create_user(username, password):
-    with open(r'Program Data\Users.txt','w+') as f:
-        f.write(username+"|"+password)
-    return 
+    found= False
+    with open(r'Program Data\Users.txt','r') as f:
+        content = f.readlines()
+        for line in content:
+            if str(line.split("|")[0]) == username:
+                found = True
+    if found == False:
+        with open(r'Program Data\Users.txt','w+') as f:
+            f.write(username+"|"+encrypt(password))
+    return
 
 def login(username, password):
     success = False
     with open(r'Program Data\Users.txt','r') as f:
         content = f.readlines()
         for line in content:
-            if line.split("|")(0) == username:
-                if decrypt(line.split("|")(1),ciphertext) == password:
+            if line.split("|")[0] == username:
+                if encrypt(password) == line.split("|")[1]:
                     success = True
     return success
-            
-
-login()
+    
+def encrypt(text):
+    chunk = ""
+    for char in text:
+        block = 255 - ord(char)
+        chunk = chunk + str(block)
+    return chunk 
+        
+main()
