03e6749ddf3a1a7a2bfba9162f84418dbb904b01
jstil003 <jstil003@plattsburgh.edu>
2015-02-26 21:58:27 -0500

    Review Updates

--- MyAttempt.py
+++ MyAttempt.py
@@ -1,62 +1,58 @@
 #!/usr/bin/env python
 
 # 2/8/15
-# Create a function that accepts a file's path, hashes the file,
-# and returns its hash.
+# Functions that handle hashing a file, searching a folder for all files of
+# that folder and it's subdirectories, and a utility function to combine the
+# file list into a dictionary with each file's hash.
 
 import hashlib, os
 
 # Global Variable(s) 
 BUFFER = 65536
 
-# INPUT: filepath 
-# OUTPUT: hash
-# Gives a hash for a given file
+# INPUT: Path of a file
+# OUTPUT: Hash of input file
+# This function takes the path of a file and returns that files hash.
 def fileHasher(path):
     ourHash = hashlib.md5()
     file = open(path, 'rb')
     while True:
         text = file.read(BUFFER)
-        if not text:
+        if not text: # text is empty
             break
         ourHash.update(text)
     file.close()
     return ourHash.hexdigest() 
 
-# INPUT: 2 hashes
-# OUTPUT: True/False
-# Gives truth value to hash value comparison
-def compareHashes(hash1, hash2):
-    return hash1 == hash2
-
-# INPUT: path of the folder
-# OUTPUT: List of files
-# Get a list of files to be hashed.
+# INPUT: Path of a folder
+# OUTPUT: List of file paths
+# Input a folder path and return all files of that folder and its possible
+# subdirectories.
 def loadFolderContent(folderPath):
     folderList = os.listdir(folderPath)
     returnList = []
     for i in folderList:
-        path = folderPath + i
+        path = folderPath + '\\' + i
         # if it is a file, add to list for hashing
         if os.path.isfile(path):
             returnList.append(path)
         # if it is a subfolder, find its file paths
         elif os.path.isdir(path):
-            subfolderList = []
-            subfolderList = loadFolderContent(path)
-            returnList += subfolderList
+            returnList += loadFolderContent(path)
     return returnList
     
 
 # INPUT: Folder path
-# OUTPUT: Dictionary with paths and their hashes
-# Helps to simplify
+# OUTPUT: Dictionary of file paths and their hashes
+# Function to find all files of a folder and subdirectories and to hash all of
+# these files and return the dictionary containing this information.
 def hashCollector(folderPath):
     hashFileDict = {}
     fileList = loadFolderContent(folderPath)
     for i in fileList:
-        ourHash = fileHasher(folderPath + i)
-        hashFileDict[folderPath+i] = ourHash
+        path = folderPath + '\\' + i
+        ourHash = fileHasher(path)
+        hashFileDict[path] = ourHash
     return hashFileDict
 
 # for specifying drive
