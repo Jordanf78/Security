3f9c8015fd2b78c6970469fdbce779a518285fd9
jstil003 <jstil003@plattsburgh.edu>
2015-03-29 02:54:19 -0400

    Some progress

--- HashFileEncryption.py
+++ HashFileEncryption.py
@@ -1,1 +1,55 @@
+#!/usr/bin/env python
 
+# 3/28/2015
+# Handles the dictionary produced from the file hashing functions and
+# handles the encryption of this file to be sent to the drive.
+
+import os, hashlib
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+
+# INPUT: dictionary (dict[filename] = hash)
+# OUTPUT: N/A
+# The filenames and hashes are stored in a file line by line.
+def storeHashes(hashDict):
+    while(not os.path.exists(r'C:\SecurityFolder')):
+        os.makedirs(r'C:\SecurityFolder')
+    file = open('C:\SecurityFolder\HashFile.txt', 'w')
+    for i in hashDict.keys():
+        # filename - hash
+        string = i + ' - ' + str(hashDict[i] + '\n')
+        file.write(string)
+    file.close()
+
+# INPUT: hash dict file
+# OUTPUT: private key or "FILEDNE" if file doesn't exist
+# The hash dict file is encrypted for transfer to the google drive.
+def encryptFile(hashFile):
+    #if os.path.exists(hashFile):
+        privateKey = RSA.generate(1024)
+        key = privateKey.publickey()
+        print(privateKey, '\n', key)
+        cipher = PKCS1_OAEP.new(key)
+        file = open(hashFile, 'r')
+        outputFile = open('C:\SecurityFolder\Encrypted.txt', 'w+b')
+        for line in file:
+            print(line)
+            outputFile.write(cipher.encrypt(line))
+        file.close()
+        outputFile.close()
+        
+    #else:
+        #return "FILEDNE"
+
+# INPUT: file, private key
+# OUTPUT: N/A
+# Decrypts the file when retrieved from the google drive.
+#def decryptFile(hashFile, key):
+
+def main():
+    d = {'C:\\Users\\Jarid\\HashFolder\\HashSubFolder\\b.txt': '81dc9bdb52d04dc20036dbd8313ed055', 'C:\\Users\\Jarid\\HashFolder\\a.txt': '912ec803b2ce49e4a541068d495ab570'}
+    storeHashes(d)
+    encryptFile(r'C:\SecurityFolder\HashFile.txt')
+
+if __name__ == "__main__":
+    main()
---
+++ HashFunctions.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+# 2/8/15
+# Functions that handle hashing a file, searching a folder for all files of
+# that folder and it's subdirectories, and a utility function to combine the
+# file list into a dictionary with each file's hash.
+
+import hashlib, os
+
+# Global Variable(s) 
+BUFFER = 65536
+
+# INPUT: Path of a file
+# OUTPUT: Hash of input file
+# This function takes the path of a file and returns that files hash.
+def fileHasher(path):
+    ourHash = hashlib.md5()
+    file = open(path, 'rb')
+    while True:
+        text = file.read(BUFFER)
+        if not text: # text is empty
+            break
+        ourHash.update(text)
+    file.close()
+    return ourHash.hexdigest() 
+
+# INPUT: Path of a folder
+# OUTPUT: List of file paths
+# Input a folder path and return all files of that folder and its possible
+# subdirectories.
+def loadFolderContent(folderPath):
+    folderList = os.listdir(folderPath)
+    returnList = []
+    for i in folderList:
+        path = folderPath + '\\' + i
+        # if it is a file, add to list for hashing
+        if os.path.isfile(path):
+            returnList.append(path)
+        # if it is a subfolder, find its file paths
+        elif os.path.isdir(path):
+            returnList += loadFolderContent(path)
+    return returnList
+    
+
+# INPUT: Folder path
+# OUTPUT: Dictionary of file paths and their hashes
+# Function to find all files of a folder and subdirectories and to hash all of
+# these files and return the dictionary containing this information.
+def hashCollector(folderPath):
+    hashFileDict = {}
+    fileList = loadFolderContent(folderPath)
+    for i in fileList:
+        ourHash = fileHasher(i)
+        hashFileDict[i] = ourHash
+    return hashFileDict
+
+# for specifying drive
+# http://stackoverflow.com/questions/827371/is-there-a-way-to-list-all-the-available-drive-letters-in-python
+
+def main():
+    print(hashCollector(r"C:\Users\Jarid\HashFolder"))
+
+if __name__ == "__main__":
+    main()
