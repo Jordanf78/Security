7061366924c791b0389bbb2ef27256065a9dbd0d
chirag3093 <chirag9330@gmail.com>
2015-04-14 21:47:15 -0400

    path.py

---
+++ path.py
@@ -0,0 +1,19 @@
+Run a Drive App in Python
+
+Step1: Enable the Drive API
+        Created Google Account
+        Go to the Google Developers Console
+        Create a project (Software)
+        Expand APIs, & auth
+        Enable API
+        Create projects credentials
+
+        Create new client ID under Oauth
+
+STEP2: Install the Google Client Library
+
+STEP3: Set up the sample
+        Created a new file document.txt
+        created Client Code(you will get CLIENT_ID and CLIENT_SECRET from creating credentials
+
+STEP4: run the code.
