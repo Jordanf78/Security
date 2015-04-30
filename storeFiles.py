d823b5f15ecf2e53c7c7c25f6255050ae4c59746
jstil003 <jstil003@plattsburgh.edu>
2015-04-30 02:52:15 -0400

    WIP

--- storeFiles.py
+++ storeFiles.py
@@ -9,8 +9,8 @@ from Crypto.PublicKey import RSA
 from random import randint
 
 # INPUT: drive for data storage, file to be encrypted
-# OUTPUT:  "FILEDNE" if file doesn't exist
-# 
+# OUTPUT: private key or "FILEDNE" if file doesn't exist
+# Encrypts the file to be stored on google drive
 def encryptFileForDrive(DRIVE, encryptFile):
     if os.path.exists(encryptFile):
         privateKey = RSA.generate(1024)
@@ -18,7 +18,8 @@ def encryptFileForDrive(DRIVE, encryptFile):
         cipher = PKCS1_OAEP.new(key)
         file = open(encryptFile, 'rb')
         randNum = randint(2**16, 2**32)
-        outputFile = open(DRIVE + r'SecurityFolder\\' + str(randNum) + '.txt', 'wb')
+        randFile = DRIVE + r'SecurityFolder\\' + str(randNum) + '.txt'
+        outputFile = open(randFile, 'wb')
         message = file.read(64)
         while message != b'':
             # use public key to encrypt 64 char messages from hashFile
@@ -26,19 +27,20 @@ def encryptFileForDrive(DRIVE, encryptFile):
             message = file.read(64)
         file.close()
         outputFile.close()
+        
         # check for same file name
         indexFile = open(DRIVE + r'SecurityFolder\index.txt', 'r')
         tempFile = open(DRIVE + r'SecurityFolder\dump', 'w')
         for line in indexFile:
             filePath, spacer, fileNum = line.partition(' - ')
             # dont copy old file name
-            print(filePath, fileNum)
             if filePath == encryptFile:
                 os.remove(DRIVE + r'SecurityFolder\\' + fileNum.strip() + '.txt')
             else:
                 tempFile.write(line)
         indexFile.close()
         tempFile.close()
+        
         # now done storing data temporarily, can move back to index file
         indexFile = open(DRIVE + r'SecurityFolder\index.txt', 'w')
         tempFile = open(DRIVE + r'SecurityFolder\dump', 'r')
@@ -48,13 +50,44 @@ def encryptFileForDrive(DRIVE, encryptFile):
         indexFile.close()
         tempFile.close()
         os.remove(DRIVE + r'SecurityFolder\dump')
+        return privateKey
     else:
         return "FILEDNE"
 
+# INPUT: drive for data storage, path of randFile, private key
+# OUTPUT: "FILEDNE" if file does not exist
+# Decrypts the file when retrieved from the google drive.
+def decryptFileForDrive(DRIVE, randFile, key):
+
+    outputFileName = ''
+    indexFile = open(DRIVE + r'SecurityFolder\index.txt', 'r')
+    for line in indexFile:
+        filePath, spacer, fileNum = line.partition(' - ')
+        print(fileNum, randFile)
+        if fileNum == randFile:
+            outputFileName = fileNum
+            print(outputFileName)
+    indexFile.close()
+        
+    #decrypt the file
+    path = DRIVE + randFile
+    if os.path.exists(path):
+        cipher = PKCS1_OAEP.new(key)
+        file = open(path, 'rb')
+        outputFile = open(outputFileName, 'wb')
+        # 128 because 2 encrypted char = 1 unencrypted char
+        message = file.read(128)
+        while message != b'':
+            outputFile.write(cipher.decrypt(message))
+            message = file.read(128)
+        file.close()
+        outputFile.close()
+    else:
+        return "FILEDNE"
 
 def main():
-    exist = encryptFileForDrive("C:\\", 'C:\\Users\\Jarid\\HashFolder\\a.txt')
-    print(exist)
+    key = encryptFileForDrive("C:\\", 'C:\\Users\\Jarid\\HashFolder\\a.txt')
+    decryptFileForDrive("C:\\", randFile, key)
 
 if __name__ == "__main__":
     main()
